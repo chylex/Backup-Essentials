4af0622d22f0cff5605c56ab7bf1edabdeb685a9
chylex <chylex@fo2.cz>
2015-04-04 00:20:58 +0200

    Added basic Backup Location page

---
+++ BackupEssentials/Backup/BackupLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackupEssentials.Backup{
+    public class BackupLocation{
+        public string Name { get; set; }
+        public string Directory { get; set; }
+
+
+    }
+}
---
+++ BackupEssentials/Pages/BackupLocations.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BackupEssentials.Backup;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace BackupEssentials.Pages{
+    public partial class BackupLocations : Page{
+        public readonly ObservableCollection<BackupLocation> BackupLocationsList = new ObservableCollection<BackupLocation>();
+
+        public BackupLocations(){
+            InitializeComponent();
+
+            // TODO uncomment once done testing
+            /*
+            BackupLocationsList.Add(new BackupLocation(){ Name = "Test1", Directory = @"C:\Folder\" });
+            BackupLocationsList.Add(new BackupLocation(){ Name = "Test2", Directory = @"C:\Folder\" });
+
+            LocationsListView.ItemsSource = BackupLocationsList;*/
+        }
+    }
+}
