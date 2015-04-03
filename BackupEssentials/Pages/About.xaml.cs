2cef6f8bb5667a36c9aadec053eecbbafc234271
chylex <chylex@fo2.cz>
2015-04-03 14:20:55 +0200

    About page with win version detection

---
+++ BackupEssentials/Pages/About.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BackupEssentials.Utils;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace BackupEssentials.Pages{
+    public partial class About : Page{
+        public About(){
+            InitializeComponent();
+
+            AboutTextVersion.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString(4);
+            
+            StringBuilder build = new StringBuilder();
+            OperatingSystem os = Environment.OSVersion;
+
+            build.Append("You are using ").Append(WindowsVersion.Get()).Append(' ');
+            build.Append(Environment.Is64BitOperatingSystem ? "x64" : "x86").Append(' ');
+            build.Append(os.ServicePack.Replace("Service Pack ","SP")).Append(' ');
+            build.Append(os.Version.Major >= 6 ? "(supported)." : "(not supported).");
+            
+            AboutTextOS.Text = build.ToString();
+        }
+
+        private void ClickLink(object sender, RequestNavigateEventArgs e){
+            Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
+        }
+    }
+}
---
+++ BackupEssentials/Utils/WindowsVersion.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace BackupEssentials.Utils{
+    static class WindowsVersion{
+        public static string Get(){
+            try{
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+                if (key != null)return (string)key.GetValue("ProductName");
+            }catch{}
+
+            return GetFromEnvironment() ?? "(unknown)";
+        }
+
+        private static string GetFromEnvironment(){
+            Version version = Environment.OSVersion.Version;
+
+            switch(version.Major){
+                case 10: return "Windows 10";
+
+                case 6:
+                    switch(version.Minor){
+                        case 0: return "Windows Vista";
+                        case 1: return "Windows 7";
+                        case 2: return "Windows 8";
+                        case 3: return "Windows 8.1";
+                    }
+
+                    break;
+
+                case 5:
+                    switch(version.Minor){
+                        case 0: return "Windows 2000";
+                        case 1: return "Windows XP";
+                        case 2: return "Windows XP";
+                    }
+
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
