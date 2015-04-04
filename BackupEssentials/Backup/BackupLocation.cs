a139e5ea30895abbb157425757fe359e632981fc
chylex <chylex@fo2.cz>
2015-04-04 22:05:24 +0200

    Forgot to push update to BackupLocation

--- BackupEssentials/Backup/BackupLocation.cs
+++ BackupEssentials/Backup/BackupLocation.cs
@@ -7,5 +7,14 @@ namespace BackupEssentials.Backup{
     public class BackupLocation{
         public string Name { get; set; }
         public string Directory { get; set; }
+
+        public void Set(BackupLocation NewData){
+            this.Name = NewData.Name;
+            this.Directory = NewData.Directory;
+        }
+
+        public BackupLocation Clone(){
+            return new BackupLocation(){ Name = this.Name, Directory = this.Directory };
+        }
     }
 }
