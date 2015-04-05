39974d0bac2953974de2f06d31015e450afa0142
chylex <chylex@fo2.cz>
2015-04-05 15:57:55 +0200

    Made the app single-instance w/ optional non-responding process kill

--- BackupEssentials/App.xaml.cs
+++ BackupEssentials/App.xaml.cs
@@ -1,12 +1,53 @@
-﻿using System.Windows;
+﻿using BackupEssentials.Utils;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace BackupEssentials{
     public partial class App : Application{
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
         private void StartApp(object sender, StartupEventArgs args){
-            MainWindow window = new MainWindow();
-            // TODO handle args
-            window.Show();
+            ProgramArgsParser parser = new ProgramArgsParser(args.Args);
+            
+            if (parser.HasFlag("runshell")){
+                
+            }
+            else{
+                Process[] running = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location));
+
+                if (running.Length > 1){
+                    int myId = Process.GetCurrentProcess().Id;
+
+                    foreach(Process process in running){
+                        if (process.Id != myId && process.MainWindowHandle != IntPtr.Zero){
+                            if (process.Responding){
+                                
+                                SetForegroundWindow(process.MainWindowHandle);
+                                Application.Current.Shutdown();
+                                return;
+                            }
+                            else{
+                                if (MessageBox.Show("The application is already running, but is not responding. Do you want to force close it?","Application is already running",MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes){
+                                    try{
+                                        process.Kill();
+                                    }catch(Exception e){
+                                        MessageBox.Show("Could not close the application: "+e.Message);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                MainWindow window = new MainWindow();
+                window.Show();
+            }
         }
 
         private void HandleException(object sender, DispatcherUnhandledExceptionEventArgs e){
