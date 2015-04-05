d0ba1f7087dfe33ad056b3d8b5f4d0e7ab1e910e
chylex <chylex@fo2.cz>
2015-04-05 15:56:48 +0200

    Added passed argument parser

---
+++ BackupEssentials/Utils/ProgramArgsParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackupEssentials.Utils{
+    class ProgramArgsParser{
+        private string[] Args;
+
+        public ProgramArgsParser(string[] args){
+            this.Args = args;
+        }
+
+        /// <summary>
+        /// Returns true if the arguments contain a flag (-&lt;flag&gt;). The dash in front is handled automatically.
+        /// </summary>
+        public bool HasFlag(string flag){
+            return Args.Contains("-"+flag);
+        }
+
+        /// <summary>
+        /// Returns the next argument after a flag (-&lt;flag&gt;), as long as it is not another flag. The dash in front is handled automatically.
+        /// </summary>
+        public string GetValue(string flag, string defaultValue){
+            flag = "-"+flag;
+
+            for(int a = 0; a < Args.Length-1; a++){
+                if (Args[a].Equals(flag)){
+                    return Args[a+1].StartsWith("-") ? defaultValue : Args[a+1];
+                }
+            }
+
+            return defaultValue;
+        }
+    }
+}
