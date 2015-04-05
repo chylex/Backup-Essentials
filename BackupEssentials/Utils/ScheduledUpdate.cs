481731a5e3a270411d447534404d544f1a8d7e26
chylex <chylex@fo2.cz>
2015-04-05 13:02:57 +0200

    Added ScheduledUpdate

---
+++ BackupEssentials/Utils/ScheduledUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace BackupEssentials.Utils{
+    class ScheduledUpdate{
+        public static ScheduledUpdate Forever(int seconds, Action update){
+            return new ScheduledUpdate(seconds,update);
+        }
+
+        public bool NeedsUpdate = false;
+        private readonly DispatcherTimer timer;
+
+        private ScheduledUpdate(int seconds, Action update){
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,seconds);
+
+            timer.Tick += (sender, args) => {
+                if (NeedsUpdate){
+                    NeedsUpdate = false;
+                    update();
+                }
+            };
+        }
+
+        public void Start(){
+            timer.Start();
+        }
+    }
+}
