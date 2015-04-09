0d1ec9bd66f3f25dda20096d908946bea406cbb3
chylex <info@chylex.com>
2015-04-09 21:26:39 +0200

    Added a (currently non-functional) window for Drag&Drop

---
+++ BackupEssentials/Pages/BackupDrop.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BackupEssentials.Backup;
+using System.Windows.Controls;
+
+namespace BackupEssentials.Pages{
+    public partial class BackupDrop : Page, IPageShowData{
+        public BackupDrop(){
+            InitializeComponent();
+
+            LocationsListView.Items.Clear();
+            LocationsListView.ItemsSource = DataStorage.BackupLocationList;
+        }
+
+        void IPageShowData.OnShow(object data){
+            string[] files = (string[])data;
+        }
+
+        private void ListViewSelectionChanged(object sender, SelectionChangedEventArgs e){
+            ButtonBackup.IsEnabled = LocationsListView.SelectedItems.Count == 1;
+        }
+    }
+}
