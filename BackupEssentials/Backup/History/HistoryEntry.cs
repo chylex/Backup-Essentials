6b103b33a23329fbfe190965a2ba5c16c67f4bc6
chylex <info@chylex.com>
2015-04-14 00:03:52 +0200

    Serialization of history entries

--- BackupEssentials/Backup/History/HistoryEntry.cs
+++ BackupEssentials/Backup/History/HistoryEntry.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using BackupEssentials.Utils;
+using System;
 
 namespace BackupEssentials.Backup.History{
-    class HistoryEntry{
+    class HistoryEntry : StringDictionarySerializer.IObjectToDictionary{
+        public string LocationName = "";
+        public DateTime BackupTime = DateTime.MinValue;
+        public int EntriesAdded = 0, EntriesUpdated = 0, EntriesRemoved = 0;
+        public string Filename = "";
+
+        void StringDictionarySerializer.IObjectToDictionary.ToDictionary(SafeDictionary<string,string> dict){
+            dict["Name"] = LocationName;
+            dict["Time"] = Convert.ToBase64String(BitConverter.GetBytes(BackupTime.ToBinary()));
+            dict["EnA"] = EntriesAdded.ToString();
+            dict["EnU"] = EntriesUpdated.ToString();
+            dict["EnR"] = EntriesRemoved.ToString();
+            dict["File"] = Filename;
+        }
+
+        void StringDictionarySerializer.IObjectToDictionary.FromDictionary(SafeDictionary<string,string> dict){
+            LocationName = dict["Name"] ?? "";
+            BackupTime = DateTime.FromBinary(BitConverter.ToInt64(Convert.FromBase64String(dict["Time"] ?? ""),0));
+            int.TryParse(dict["EnA"] ?? "0",out EntriesAdded);
+            int.TryParse(dict["EnU"] ?? "0",out EntriesUpdated);
+            int.TryParse(dict["EnR"] ?? "0",out EntriesRemoved);
+            Filename = dict["File"] ?? "";
+        }
     }
 }
