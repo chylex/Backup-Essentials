bad72a9e9bd335689fad795491c75cf0e26fc6bc
chylex <info@chylex.com>
2015-04-16 17:56:44 +0200

    Made HistoryEntry xaml-friendly

--- BackupEssentials/Backup/History/HistoryEntry.cs
+++ BackupEssentials/Backup/History/HistoryEntry.cs
@@ -2,11 +2,19 @@
 using System;
 
 namespace BackupEssentials.Backup.History{
-    class HistoryEntry : StringDictionarySerializer.IObjectToDictionary{
-        public string LocationName = "";
-        public DateTime BackupTime = DateTime.MinValue;
-        public int EntriesAdded = 0, EntriesUpdated = 0, EntriesRemoved = 0;
+    public class HistoryEntry : StringDictionarySerializer.IObjectToDictionary{
+        public string LocationName { get; set; }
+        public DateTime BackupTime { get; set; }
+        public int EntriesAdded { get; set; }
+        public int EntriesUpdated { get; set; }
+        public int EntriesRemoved { get; set; }
         public string Filename = "";
+
+        public HistoryEntry(){
+            LocationName = "";
+            BackupTime = DateTime.MinValue;
+            EntriesAdded = EntriesUpdated = EntriesRemoved = 0;
+        }
 
         void StringDictionarySerializer.IObjectToDictionary.ToDictionary(SafeDictionary<string,string> dict){
             dict["Name"] = LocationName;
@@ -20,9 +28,15 @@ namespace BackupEssentials.Backup.History{
         void StringDictionarySerializer.IObjectToDictionary.FromDictionary(SafeDictionary<string,string> dict){
             LocationName = dict["Name"] ?? "";
             BackupTime = DateTime.FromBinary(BitConverter.ToInt64(Convert.FromBase64String(dict["Time"] ?? ""),0));
-            int.TryParse(dict["EnA"] ?? "0",out EntriesAdded);
-            int.TryParse(dict["EnU"] ?? "0",out EntriesUpdated);
-            int.TryParse(dict["EnR"] ?? "0",out EntriesRemoved);
+
+            int enAdd = 0, enUpd = 0, enRem = 0;
+            int.TryParse(dict["EnA"] ?? "0",out enAdd);
+            int.TryParse(dict["EnU"] ?? "0",out enUpd);
+            int.TryParse(dict["EnR"] ?? "0",out enRem);
+            EntriesAdded = enAdd;
+            EntriesUpdated = enUpd;
+            EntriesRemoved = enRem;
+
             Filename = dict["File"] ?? "";
         }
     }
