c47f37c37b358db9a434cf2443abb4004276008c
chylex <info@chylex.com>
2015-04-19 13:31:35 +0200

    Added DisplaySetting class for Settings screen

---
+++ BackupEssentials/Sys/UI/DisplaySetting.cs
@@ -0,0 +1,11 @@
+﻿namespace BackupEssentials.Sys.UI{
+    public class DisplaySetting<T>{
+        public T Value { get; private set; }
+        public string Display { get; private set; }
+
+        public DisplaySetting(T value, string display){
+            this.Value = value;
+            this.Display = display;
+        }
+    }
+}
