ac8c0822db8cd07f4712249c314dcce8c36a66e4
chylex <info@chylex.com>
2015-04-19 13:30:42 +0200

    Added Number Serialization class

---
+++ BackupEssentials/Utils/NumberSerialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackupEssentials.Utils{
+    static class NumberSerialization{
+        public static string WriteInt(int value){
+            return Convert.ToBase64String(BitConverter.GetBytes(value));
+        }
+
+        public static string WriteLong(long value){
+            return Convert.ToBase64String(BitConverter.GetBytes(value));
+        }
+
+        public static int ReadInt(string text){
+            return BitConverter.ToInt32(Convert.FromBase64String(text),0);
+        }
+
+        public static long ReadLong(string text){
+            return BitConverter.ToInt64(Convert.FromBase64String(text),0);
+        }
+    }
+}
