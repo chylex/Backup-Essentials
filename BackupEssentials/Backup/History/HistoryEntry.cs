79f8101de33b1fcdcb98542a52efbab367c72840
chylex <info@chylex.com>
2015-04-19 13:31:12 +0200

    Updated HistoryEntry to use number serialization

--- BackupEssentials/Backup/History/HistoryEntry.cs
+++ BackupEssentials/Backup/History/HistoryEntry.cs
@@ -20,7 +20,7 @@ namespace BackupEssentials.Backup.History{
 
         void StringDictionarySerializer.IObjectToDictionary.ToDictionary(SafeDictionary<string,string> dict){
             dict["Name"] = LocationName;
-            dict["Time"] = Convert.ToBase64String(BitConverter.GetBytes(BackupTime.ToBinary()));
+            dict["Time"] = NumberSerialization.WriteLong(BackupTime.ToBinary());
             dict["EnA"] = EntriesAdded.ToString();
             dict["EnU"] = EntriesUpdated.ToString();
             dict["EnR"] = EntriesRemoved.ToString();
@@ -29,7 +29,7 @@ namespace BackupEssentials.Backup.History{
 
         void StringDictionarySerializer.IObjectToDictionary.FromDictionary(SafeDictionary<string,string> dict){
             LocationName = dict["Name"] ?? "";
-            BackupTime = DateTime.FromBinary(BitConverter.ToInt64(Convert.FromBase64String(dict["Time"] ?? ""),0));
+            BackupTime = DateTime.FromBinary(NumberSerialization.ReadLong(dict["Time"] ?? ""));
 
             int enAdd = 0, enUpd = 0, enRem = 0;
             int.TryParse(dict["EnA"] ?? "0",out enAdd);
