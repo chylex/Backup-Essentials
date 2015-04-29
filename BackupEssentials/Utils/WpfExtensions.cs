a64f4574a689199d9c77a45d822c96c8840ab741
chylex <info@chylex.com>
2015-04-29 17:25:55 +0200

    Added binding updating extension method

---
+++ BackupEssentials/Utils/WpfExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BackupEssentials.Utils{
+    static class WpfExtensions{
+        public static IEnumerable<DependencyObject> EnumerateMeAndChildren(this DependencyObject obj){
+            yield return obj;
+
+            for(int a = 0; a < VisualTreeHelper.GetChildrenCount(obj); a++){
+                foreach(DependencyObject child in VisualTreeHelper.GetChild(obj,a).EnumerateMeAndChildren()){
+                    yield return child;
+                }
+            }
+        }
+
+        public static void UpdateBindings(this DependencyObject parent){
+            foreach(DependencyObject obj in parent.EnumerateMeAndChildren()){
+                LocalValueEnumerator enumerator = obj.GetLocalValueEnumerator();
+
+                while(enumerator.MoveNext()){
+                    BindingExpressionBase binding = BindingOperations.GetBindingExpressionBase(obj,enumerator.Current.Property);
+                    if (binding != null)binding.UpdateTarget();
+                }
+            }
+        }
+    }
+}
