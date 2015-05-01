c412401ce1ca9b6c617d73cab9057afe3c64e0dd
chylex <info@chylex.com>
2015-05-01 21:56:34 +0200

    Separated Home page into a multiple pages

--- BackupEssentials/Pages/Home.xaml.cs
+++ BackupEssentials/Pages/Home.xaml.cs
@@ -1,12 +1,18 @@
-﻿using BackupEssentials.Utils;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace BackupEssentials.Pages{
     public partial class Home : Page{
         public Home(){
             InitializeComponent();
+            HomePageFrame.Navigated += (sender2, args2) => { HomePageFrame.NavigationService.RemoveBackEntry(); };
+            ChangeScreen(BackupGuide,new RoutedEventArgs());
+        }
 
-            if (!WindowsVersion.IsFullySupported())TextBlockContextMenuWarning.Opacity = 1;
+        private void ChangeScreen(object sender, RoutedEventArgs e){
+            Type pageType = GetType().Assembly.GetType("BackupEssentials.Pages.HomePages."+((RadioButton)sender).Name,false);
+            HomePageFrame.Content = pageType == null ? null : AppPageManager.GetPage(pageType);
         }
     }
 }
---
+++ BackupEssentials/Pages/HomePages/BackupGuide.xaml.cs
@@ -0,0 +1,12 @@
+﻿using BackupEssentials.Utils;
+using System.Windows.Controls;
+
+namespace BackupEssentials.Pages.HomePages{
+    public partial class BackupGuide : Page{
+        public BackupGuide(){
+            InitializeComponent();
+
+            if (!WindowsVersion.IsFullySupported())TextBlockContextMenuWarning.Opacity = 1;
+        }
+    }
+}
