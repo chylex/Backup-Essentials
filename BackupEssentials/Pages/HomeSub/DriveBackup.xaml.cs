7933151249b5251668a0de768e7f728a567a6eb8
chylex <info@chylex.com>
2015-05-05 23:04:19 +0200

    Work on Home texts

---
+++ BackupEssentials/Pages/HomeSub/DriveBackup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BackupEssentials.Pages.HomeSub{
+    public partial class DriveBackup : Page{
+        public DriveBackup(){
+            InitializeComponent();
+        }
+
+        private void OpenMyComputer(object sender, RoutedEventArgs e){
+            Process.Start("::{20d04fe0-3aea-1069-a2d8-08002b30309d}");
+        }
+    }
+}
