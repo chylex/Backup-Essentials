aecdcfc68f4e26d1b4a48d853177cc71a152239d
chylex <info@chylex.com>
2015-05-16 17:01:58 +0200

    Properly tested history gen, removed the TODOs

--- BackupEssentials/Backup/History/HistoryUtils.cs
+++ BackupEssentials/Backup/History/HistoryUtils.cs
@@ -5,18 +5,15 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace BackupEssentials.Backup.History{
-    static class HistoryUtils{ // TODO test
+    static class HistoryUtils{
         public static List<HistoryEntry> RemoveOldEntriesFromList(){
             List<HistoryEntry> list = new List<HistoryEntry>();
             int entriesKept = Settings.Default.HistoryEntriesKept.Value, index;
 
             if (entriesKept != -1){
-                App.LogInfo("== Trying to remove old entries... current "+DataStorage.HistoryEntryList.Count+", target "+entriesKept); // TODO
-
                 while(DataStorage.HistoryEntryList.Count > entriesKept){
                     list.Add(DataStorage.HistoryEntryList[index = DataStorage.HistoryEntryList.Count-1]);
                     DataStorage.HistoryEntryList.RemoveAt(index);
-                    App.LogInfo("Removing... current "+DataStorage.HistoryEntryList.Count); // TODO
                 }
             }
 
@@ -29,7 +26,6 @@ namespace BackupEssentials.Backup.History{
             foreach(HistoryEntry entry in entries){
                 try{
                     string file = Path.Combine(HistoryEntry.Directory,entry.Filename);
-                    App.LogInfo("Deleting entry file... "+entry.Filename); // TODO
                     if (File.Exists(file))File.Delete(file);
                 }catch(Exception e){
                     App.LogException(e);
